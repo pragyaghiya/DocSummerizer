e4c2389f6dab89ae52ae45cae209661c93d7ecf3
Pragya Ghiya <pragyaghiya123@gmail.com>
2021-06-03 23:37:01 +0530

    huluulu

--- login/src/App.js
+++ login/src/App.js
@@ -1,35 +1,26 @@
-import logo from './logo.svg';
-import React, { Component } from 'react';
-import './App.css';
-import { useState,useEffect } from 'react';
-import fire from './fire';
-import Login from './login';
-import Home from './home';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState, useEffect } from "react";
+import fire from "./fire";
+import Login from "./login";
+import Home from "./home";
 
-const App = ()=>{
+const App = () => {
   const [user, setUser] = useState();
-
 
   const handleLogout = () => {
     fire.auth().signOut();
-};
+  };
 
-  return(
+  return (
     <div className="App">
-    {user ? (
-      <Home handleLogout={handleLogout} />
-    ) : (
-      <Login 
-         user={user}
-         setUser={setUser}
-        
-        />
-        
-    )}
-    
+      {user ? (
+        <Home handleLogout={handleLogout} />
+      ) : (
+        <Login user={user} setUser={setUser} />
+      )}
     </div>
-
-  )
-}
+  );
+};
 
 export default App;
